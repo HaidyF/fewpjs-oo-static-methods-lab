e655b4be8bacd087200a32ea8aadd4715ea6db7d
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-03-25 23:42:03 -0400

    commit tree bf86ce903bd69d18729151014c89c3c9f5522039

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(str){
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(str){
+    const small = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return str.split(" ").map((word, i)=> small.includes(word) && i !== 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+  }
 }
